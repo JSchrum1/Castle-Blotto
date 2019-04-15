e3d6d4a2f9d3832c46ad4fc3e85ebbe12825104e
JSchrum1 <48635781+JSchrum1@users.noreply.github.com>
2019-04-15 18:46:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     playerList=makeList(starterPlayers())
     PrintRec(playerList)
     #printPlayers(playerList)
-    #groupList=["Statler's", "Penn State", "GLVB"]
+    #groupList=["Work", "College", "Sports"]
     printPlayer1v1(playerList, 119)
     #printAll1v1AVGs(playerList)
 
@@ -44,24 +44,24 @@ def starterPlayers():
     PL=[]
     # Name,                Castle #: 1   2   3   4   5   6   7   8   9  10,  groups
     PL.append(p("Joey Schrum",       2,  2,  2,  2, 21,  2, 33, 33,  2,  1,  [1,2,3]))
-    PL.append(p("Adam Wolford",      0,  0,  2,  5, 10, 10, 15, 18, 20, 20,  [2]))
-    PL.append(p("Aaron Harter",      0,  0,  0,  0,  0, 20, 20, 20, 20, 20,  [1]))
-    PL.append(p("Carson Palmer",     0,  0,  0,  0,  0, 34, 33, 33,  0,  0,  [2]))
-    PL.append(p("Tommy Labick",      5,  0,  0, 15, 20, 20, 20, 20,  0,  0,  [1,3]))
-    PL.append(p("Anna Hu",           0,  0,  0,  0, 22,  0, 39, 39,  0,  0)   )
-    PL.append(p("Jake Etling",       0,  0,  0,  0, 24, 26, 10,  0, 40,  0,  [2]))
-    PL.append(p("Dylan Stephenson",  3,  5,  8, 10, 13,  1, 26, 30,  2,  2)   )
-    PL.append(p("Tyler Zellers",     15, 5, 15,  5, 15,  5, 15,  5, 15,  5,  [1]))
-    PL.append(p("Mason Petrosky",    1,  1,  1,  1,  1, 24, 34, 36,  1,  0,  [2]))
-    PL.append(p("Katie Bagdon",      10, 3, 36,  1,  1,  1,  0, 46,  1,  1,  [1]))
-    PL.append(p("Nick Pickard",      0,  0,  0, 25, 25, 25, 25,  0,  0,  0,  [3]))
-    PL.append(p("Dom Ulicne",        0,  0,  0,  0,  0, 34, 33, 33,  0,  0,  [1]))
-    PL.append(p("Reed Fenton",       3,  3,  4,  0,  0, 30, 30, 30,  0,  0,  [3]))
-    PL.append(p("Alex Ryan",         0,  0, 10,  0,  0, 25, 25, 40,  0,  0,  [3]))
-    PL.append(p("Alec Spehar",       1,  1,  1,  0, 31,  0, 31, 31,  0,  4))
-    PL.append(p("Dan Statler",       2,  2,  4,  6, 23, 21, 21, 21,  0,  0,  [1]))
-    PL.append(p("Jarred Vasinko",   15, 15, 15, 15, 15, 15, 10,  0,  0,  0,  [2]))
-    PL.append(p("Jasmine Ferrence",  0,  0, 15, 15, 10, 20, 11, 11, 17,  1))
+    PL.append(p("Adam",      0,  0,  2,  5, 10, 10, 15, 18, 20, 20,  [2]))
+    PL.append(p("Aaron",      0,  0,  0,  0,  0, 20, 20, 20, 20, 20,  [1]))
+    PL.append(p("Carson",     0,  0,  0,  0,  0, 34, 33, 33,  0,  0,  [2]))
+    PL.append(p("Tommy",      5,  0,  0, 15, 20, 20, 20, 20,  0,  0,  [1,3]))
+    PL.append(p("Anna",           0,  0,  0,  0, 22,  0, 39, 39,  0,  0)   )
+    PL.append(p("Jake",       0,  0,  0,  0, 24, 26, 10,  0, 40,  0,  [2]))
+    PL.append(p("Dylan",  3,  5,  8, 10, 13,  1, 26, 30,  2,  2)   )
+    PL.append(p("Tyler",     15, 5, 15,  5, 15,  5, 15,  5, 15,  5,  [1]))
+    PL.append(p("Mason",    1,  1,  1,  1,  1, 24, 34, 36,  1,  0,  [2]))
+    PL.append(p("Katie",      10, 3, 36,  1,  1,  1,  0, 46,  1,  1,  [1]))
+    PL.append(p("Nick",      0,  0,  0, 25, 25, 25, 25,  0,  0,  0,  [3]))
+    PL.append(p("Dom",        0,  0,  0,  0,  0, 34, 33, 33,  0,  0,  [1]))
+    PL.append(p("Reed",       3,  3,  4,  0,  0, 30, 30, 30,  0,  0,  [3]))
+    PL.append(p("Alex",         0,  0, 10,  0,  0, 25, 25, 40,  0,  0,  [3]))
+    PL.append(p("Alec",       1,  1,  1,  0, 31,  0, 31, 31,  0,  4))
+    PL.append(p("Dan",       2,  2,  4,  6, 23, 21, 21, 21,  0,  0,  [1]))
+    PL.append(p("Jarred",   15, 15, 15, 15, 15, 15, 10,  0,  0,  0,  [2]))
+    PL.append(p("Jasmine",  0,  0, 15, 15, 10, 20, 11, 11, 17,  1))
     return PL
 
 if __name__=="__main__":

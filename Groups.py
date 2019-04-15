38480c4d641d292a4ed8583371c973d01ecc2b12
JSchrum1 <48635781+JSchrum1@users.noreply.github.com>
2019-04-15 19:07:26 -0400

    Update Groups.py

--- Groups.py
+++ Groups.py
@@ -1,5 +1,11 @@
 import player
 
+# By Joe Schrum
+# Email JoeSchrum1@gmail.com with questions or comments
+
+#WARNING: The groups class may be buggy and has limited functionality as I did not actually use it so I did very little testing
+
+#Returns a list containing only players belonging to that group given the list of all players and their groupID
 def makeGroup(playerList, groupNum):
     group=[]
     for player in playerList:
@@ -8,6 +14,7 @@ def makeGroup(playerList, groupNum):
                 group.append(player)
     return group
 
+#Prints name and list of players in a certain group
 def printGroup(playerList, groupNum, groupList):
     group=makeGroup(playerList, groupNum)
     try:
@@ -16,6 +23,7 @@ def printGroup(playerList, groupNum, groupList):
         print("Unnamed Group #{}".format(groupNum))
     player.printPlayerList(group)
 
+#Creates a new group and allows you to name it
 def createGroup(groupList, name="NULL"):
     if(name=="NULL"):
         name=input("Enter group name: ")
